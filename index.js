d833a94b767910ab67ccdc6aab134304c44e9bdf
Austin Mayden <austin.mayden@gmail.com>
2018-07-10 21:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function theBeatlesPlay (musicians, instruments) {
   var musicianString;
   var musicianInstrument=[];
   for (var i = o; i < musicians.length; i++) {
-    
+    musicianString = musicians[i] + 'plays ' + instruments[i];
+    musicianInstrument[i]=musicianString;
   }
   return musicianInstrument;
 }
@@ -12,5 +13,5 @@ function johnLennonFacts (facts) {
 }
 
 function iLoveTheBeatles (number) {
-  
+
 }
