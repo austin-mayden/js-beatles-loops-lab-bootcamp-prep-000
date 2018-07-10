1cb5742f50e7c31b6c054a1609a105814e708254
Austin Mayden <austin.mayden@gmail.com>
2018-07-10 21:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(factsArray) {
   var i = 0;
   var newArray=[];
   while(facts.length > i) {
-    newArray.push(`${factsArray[i]} = "!!!"`);
+    newArray.push(`${factsArray[i]} + "!!!"`);
     i++;
   }
   return newArray;
